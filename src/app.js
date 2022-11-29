f1926acd66bd87cd1ea0380656a7356b5957325b
alexis210197 <ale_07_chuli@hotmail.com>
2022-11-29 17:42:04 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,38 @@
 const express = require('express')
 
+const db = require('./utils/database')
+
+const userRouter = require('./users/users.router')
+
 const port = 9000
 
-const app = express
+const app = express()
+
+db.authenticate()
+    .then(()=>{
+        console.log('Database Authenticated!');
+    })
+    .catch(() =>{
+        console.log(err);
+    })
+
+    db.sync()
+    .then(()=>{
+        console.log('Database Synced');
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Ok!'})
 }) 
 
-app.listen(() => {
+app.use('/api/v1', userRouter)
+
+
+app.listen(port, () => {
     console.log(`Server started at port ${port}`)
 })
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,2 +1,13 @@
+const {Sequelize} = require('sequelize')
 
+const db = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
+    database: 'usersEntregable',
+    username: 'postgres',
+    password:'csir1913',
+    port: 5432
+})
+
+module.exports = db
 
